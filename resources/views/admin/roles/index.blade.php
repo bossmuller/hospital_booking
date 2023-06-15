dbe108c231f8e37222f6caf29f2e711b3ab43eb2
Bossvill Witaba <Bossvill Imbosa@Bossvill.strathmore.local>
2023-06-15 17:39:15 +0300

    first commit

---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,43 @@
+<x-admin-layout>
+
+
+    <div class="py-12 w-full">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-2">
+                <div class="flex justify-end p-2">
+                    <a href="" class="px-4 bg-green-700 hover:bg-green-500 rounded-md">Create Role</a>
+                </div>
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Action
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                        @foreach($roles as $role)
+                        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                           <td class="px-6 py-4 whitespace-nowrap">
+                               <div class="flex items-center">
+                                   {{@$role->name}}
+                               </div>
+                           </td>
+                            <td class="px-6 py-4">
+                                <a href="">Edit</a>
+                                <a href="">Delete</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
---
+++ resources/views/components/admin-links.blade.php
@@ -0,0 +1,9 @@
+@props(['active'])
+
+@php
+$classes = ($active ?? false)
+            ?'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:text-gray-900 hover: bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline'
+            : 'block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark: bg-transparent hover: text-gray-900 focus: text-gray-900 '
+
+
+ @endphp
